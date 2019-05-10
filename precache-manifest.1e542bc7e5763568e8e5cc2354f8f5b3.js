100092169dd3d57759ee3c80e97c518b4b6ad5a4
muneraabdul <Muneraabdul96@hotmail.com>
2019-05-10 14:32:29 +0300

    Updates

---
+++ precache-manifest.1e542bc7e5763568e8e5cc2354f8f5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2def4a7d93c27a3337a7b625649f3446",
+    "url": "/Make_History/index.html"
+  },
+  {
+    "revision": "a3ebbd5896e33f11378e",
+    "url": "/Make_History/static/css/main.7ff0e5bb.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Make_History/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "a3ebbd5896e33f11378e",
+    "url": "/Make_History/static/js/main.4fee7171.chunk.js"
+  },
+  {
+    "revision": "10668f22063ca69d0cc1",
+    "url": "/Make_History/static/js/runtime~main.d523c575.js"
+  },
+  {
+    "revision": "b357a1e37bd10a5d6f1310e017d4777a",
+    "url": "/Make_History/static/media/diiis.b357a1e3.png"
+  },
+  {
+    "revision": "1528ee625420e9ce19db8e91ec6cfee4",
+    "url": "/Make_History/static/media/fire.1528ee62.jpg"
+  }
+]);
